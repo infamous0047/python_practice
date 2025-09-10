9445fd79f9858a743a75b04f4fc63d5be2c61fbe
infamous0047 <205036365+infamous0047@users.noreply.github.com>
2025-09-10 10:08:20 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,10 @@
+print("please enter number")
+a = input()
+print("please enter another number: ")
+b = input()
+print("please enter another another number")
+c = input()
+
+d = int(a)+int(b)+int(c)
+print(d)
+
